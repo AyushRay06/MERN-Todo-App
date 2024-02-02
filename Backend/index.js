1e3ca7f28c2f35bb72d0f5e676c43f9c7269c5ba
Ayush Ray <rayayush@gmail.com>
2024-02-02 17:10:14 +0530

    Input-Validation added

--- Backend/index.js
+++ Backend/index.js
@@ -1,31 +1,47 @@
-const express = require("express");
-//const { createTodo, updateTodo } = require("./types");
-const { Todos } = require("./db");
-const cors = require("cors");
-const port = 3000;
-const app = express();
-app.use(cors());
-app.use(express.json());
+const express = require("express")
+const { createTodo, updateTodo } = require("./types")
+const { Todos } = require("./db")
+const cors = require("cors")
+const port = 3000
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 app.post("/todo", async (req, res) => {
+  const createPayload = req.body
+  const parsedPayload = createTodo.safeParse(createPayload)
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "Invalid-Input",
+    })
+    return
+  }
+
   await Todos.create({
     title: req.body.title,
     description: req.body.description,
     completed: false,
-  });
+  })
   res.json({
     msg: "Todo Created Successfuly",
-  });
-});
+  })
+})
 
 app.get("/todos", async (req, res) => {
-  const todos = await Todos.find({});
+  const todos = await Todos.find({})
   res.json({
     todos,
-  });
-});
+  })
+})
 
 app.put("/completed", (req, res) => {
+  const udatePayload = req.body
+  const parsedPayload = updateTodo.safeParse(udatePayload)
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "Invalid Input",
+    })
+  }
   Todos.update(
     {
       _id: req.body._id,
@@ -33,12 +49,12 @@ app.put("/completed", (req, res) => {
     {
       completed: true,
     }
-  );
+  )
   res.json({
     msg: "todo updated successfuly",
-  });
-});
+  })
+})
 
 app.listen(port, () => {
-  console.log(`App runnng on ${port}`);
-});
+  console.log(`App runnng on ${port}`)
+})
